8a2c0eafac8ec7347d8ba85dd0993e40fa15998d
learnerdilip <diliptalks@gmail.com>
2020-02-08 12:13:56 +0100

    database working with pre-defined 3 rows

--- sequelize-rest.js
+++ sequelize-rest.js
@@ -1,14 +1,8 @@
 const Sequelize = require("sequelize");
 
-const app = require("./messages-api");
-
 const databaseUrl = "postgres://postgres:secret@localhost:5432/postgres";
 
 const db = new Sequelize(databaseUrl);
-
-db.sync()
-  .then(() => console.log("Databse Connected"))
-  .catch(err => console.error("ERROR"));
 
 const Movie = db.define("movie", {
   title: Sequelize.STRING,
@@ -16,18 +10,35 @@ const Movie = db.define("movie", {
   synopsis: Sequelize.STRING
 });
 
-Movie.create({
-  title: "The Godfather",
-  yearOfRelease: 1980,
-  synopsis: "One of the best movies"
-});
+db.sync()
+  .then(() => console.log("Databse Connected"))
+  .then(() =>
+    Promise.all([
+      Movie.create({
+        title: "The Godfather",
+        yearOfRelease: 1980,
+        synopsis: "One of the best movies"
+      }),
+      Movie.create({
+        title: "The Dark Knight",
+        yearOfRelease: 2010,
+        synopsis: "One of the best"
+      }),
+      Movie.create({
+        title: "The Goodwill Hunting",
+        yearOfRelease: 2014,
+        synopsis: "best movies"
+      })
+    ])
+  )
+  .catch(err => console.error("ERROR"));
 
-app.get("/:movie", (req,res, next) => {
-  Movie.findOne({
-    where: {
-      req.body.title === `${movie}`
-    }
-  })
-    .then(movie => res.send(movie))
-    .catch(err => next(err))
-})
+// app.get("/:movie", (req,res, next) => {
+//   Movie.findOne({
+//     where: {
+//       req.body.title === `${movie}`
+//     }
+//   })
+//     .then(movie => res.send(movie))
+//     .catch(err => next(err))
+// })
